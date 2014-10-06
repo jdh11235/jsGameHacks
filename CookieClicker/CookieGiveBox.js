ca77e44275adbec555ee375f613497f99dc0b7e8
Jonathan Herman <jdh11235@gmail.com>
2014-10-06 16:53:11 -0500

    change to keypress-based status updates

--- CookieClicker/CookieGiveBox.js
+++ CookieClicker/CookieGiveBox.js
@@ -1,41 +1,51 @@
 var CookieGiveBoxProto = Object.create(HTMLInputElement.prototype);
 
 CookieGiveBoxProto.createdCallback = function() {
-  var self = this;
+  var elm = this;
 
-  self.style.position = 'fixed';
-  self.style.bottom = '0em';
-  self.style.right = '0em';
-  self.style.backgroundColor = 'white';
+  elm.style.position = 'fixed';
+  elm.style.bottom = '0em';
+  elm.style.right = '0em';
+  elm.style.backgroundColor = 'white';
 
-  function flash(color) {
-    self.style.backgroundColor = color;
-
-    function setWhite() { self.style.backgroundColor = 'white'; }
-    setTimeout(setWhite, 300);
+  function status(color) {
+    elm.style.backgroundColor = color;
   }
 
-  function error(state) {
-    if (state == true) {
-      flash('red');
+  function enter() {
+    var n = +elm.value;
+    function isValid(n) {
+      if ( !isNaN(n) ) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
-    if (state == false) {
-      flash('lime');
-    }
-  }
-
-  function addCookies() {
-    var n = self.value;
-    if ( !isNaN(+n) ) {
-      Game.Earn(+n);
-      error(false);
+    if ( isValid(n) ) {
+      Game.Earn(n);
+      status('lime');
     } else {
-      error(true);
+      status('red');
     }
   }
 
-  self.addEventListener('change', addCookies);
+  function reset() {
+    status('white');
+  }
+
+  function keyHandler(event) {
+    var key = event.keyCode;
+    if (key == 13) { //enter
+      enter();
+    }
+
+    if (key == 8 || key == 46) { //backspace or delete
+      reset();
+    }
+  }
+
+  elm.addEventListener('keydown', keyHandler);
 };
 
 var CookieGiveBox = document.registerElement('hacks-cookiegivebox', {
