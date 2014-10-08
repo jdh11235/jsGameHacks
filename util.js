c189afe5af140851d2c140e93d843459c7122b1f
Jonathan Herman <jdh11235@gmail.com>
2014-10-08 12:41:36 -0500

    automatically convert marklet-box divs to marklets

--- util.js
+++ util.js
@@ -7,6 +7,27 @@ var util = {
   },
 
   formatMarklet: function(text) {
-    return 'javascript:{' + text.replace(/[\040\n]/gi, '%20'); + '};void(0)';
+    return 'javascript:{' + text.replace(/\s/gi, '%20'); + '};void(0)';
+  },
+
+  importMarklet: function(url, elm) {
+    var marklet = util.formatMarklet( util.fetchTextFile(url + '.js') );
+    elm.href = marklet;
+    elm.innerHTML = url.replace(/^.+\//gi, '');
+    return elm;
+  },
+
+  autoMarkletBoxes: function() {
+    var containers = document.getElementsByClassName('marklet-box');
+    for (var i = 0; i <= containers.length; i++) {
+      util.importMarklet(containers[i].innerHTML, containers[i]);
+    }
+  },
+
+  init: function() {
+    util.autoMarkletBoxes();
   }
 };
+
+
+window.addEventListener('load', util.init);
