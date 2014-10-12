d69e699bb6e7f504e054b3603cd42ae3aae1248b
Jonathan Herman <jdh11235@gmail.com>
2014-10-12 17:50:16 -0500

    rewrite object oriented, activating twice toggles

--- CookieClicker/GiveBox.js
+++ CookieClicker/GiveBox.js
@@ -5,62 +5,70 @@ if (!jsGameHacks) {
   var jsGameHacks = {};
 }
 
-(function(){
+if (!jsGameHacks.GiveBox) {
+  jsGameHacks.GiveBox = {
+    createBox: function() {
+      this.box = document.createElement('input');
 
-  var box = document.createElement('input');
+      this.box.style.position = 'fixed';
+      this.box.style.zIndex = '1000';
+      this.box.style.bottom = '0em';
+      this.box.style.right = '0em';
+      this.box.style.backgroundColor = 'white';
+      this.box.placeholder = 'GiveBox';
+      this.box.addEventListener('keydown', jsGameHacks.GiveBox.keyHandler);
 
-  box.style.position = 'fixed';
-  box.style.zIndex = '1000';
-  box.style.bottom = '0em';
-  box.style.right = '0em';
-  box.style.backgroundColor = 'white';
-  box.placeholder = 'GiveBox';
+      document.body.appendChild(this.box);
+    },
 
-  function status(color) {
-    box.style.backgroundColor = color;
-  }
+    status: function(color) {
+      this.box.style.backgroundColor = color;
+    },
 
-  function enter() {
-    var n = +box.value;
+    enter: function() {
+      var n = +this.box.value;
 
-    function isValid(n) {
-      if ( !isNaN(n) ) {
-        return true;
-      } else {
-        return false;
+      function isValid(n) {
+        if ( !isNaN(n) ) {
+          return true;
+        } else {
+          return false;
+        }
       }
+
+      if ( isValid(n) ) {
+        Game.Earn(n);
+        jsGameHacks.GiveBox.status('lime');
+      } else {
+        jsGameHacks.GiveBox.status('red');
+      }
+    },
+
+    resetStatus: function() {
+      jsGameHacks.GiveBox.status('white');
+    },
+
+    keyHandler: function(event) {
+      var key = event.keyCode;
+
+      if (key == 13) { //enter
+        jsGameHacks.GiveBox.enter();
+      } else if (key == 27) { //esc
+        jsGameHacks.GiveBox.remove();
+      } else {
+        jsGameHacks.GiveBox.resetStatus();
+      }
+    },
+
+    remove: function() {
+      this.box.parentNode.removeChild(this.box);
+      jsGameHacks.GiveBox = undefined;
     }
 
-    if ( isValid(n) ) {
-      Game.Earn(n);
-      status('lime');
-    } else {
-      status('red');
-    }
-  }
+  };
 
-  function reset() {
-    status('white');
-  }
-
-  function keyHandler(event) {
-    var key = event.keyCode;
-
-    if (key == 13) { //enter
-      enter();
-    } else {
-      reset();
-    }
-  }
-
-  box.addEventListener('keydown', keyHandler);
-
-  if (jsGameHacks.activeElement) {
-    jsGameHacks.activeElement.parentNode.removeChild(jsGameHacks.activeElement);
-  }
-
-  jsGameHacks.activeElement = document.body.appendChild(box);
-  box.focus();
-
-})();
-
+  jsGameHacks.GiveBox.createBox();
+  jsGameHacks.GiveBox.box.focus();
+} else {
+  jsGameHacks.GiveBox.remove();
+}
